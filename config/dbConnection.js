93c18d76b64ff4d4f0390faf2d4a28e68f2505b5
NARENDRA9959 <cnarendra329@gmail.com>
2024-05-09 01:22:15 +0530

    done

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const dotenv = require("dotenv").config();
 
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "127.0.0.1",
+  user: "eloanspro",
+  password: "Eloanspro@2024",
+  database: "eloanspro",
   multipleStatements: true
 };
 
