a14debe81ba18cfa2699163e42e06181e9e260dd
kalyonnii <mudhiiguubbakalyonnii@gmail.com>
2024-05-10 11:00:51 +0530

    "Add error handling and rollback for creating lead and inserting leadId into leaddocuments table"
    This diff includes changes to the 'createLead' function in the 'leadsController.js' file. It adds error handling

--- controllers/leadsController.js
+++ controllers/leadsController.js
@@ -168,6 +168,50 @@ const addDocumentData = asyncHandler((req, res) => {
 //   });
 // });
 
+// const createLead = asyncHandler((req, res) => {
+//   let leadId = "L-" + generateRandomNumber(6);
+//   req.body["leadId"] = leadId;
+//   req.body["leadInternalStatus"] = 1;
+//   req.body["lastLeadInternalStatus"] = 1;
+//   const createClause = createClauseHandler(req.body);
+//   const sql = `INSERT INTO leads (${createClause[0]}) VALUES (${createClause[1]})`;
+//   dbConnect.query(sql, (err, result) => {
+//     if (err) {
+//       console.log("Create lead  error:");
+//     }
+//     const id = result.insertId;
+//     // Insert leadId into leaddocuments table
+//     const leaddocumentsSql = `INSERT INTO leaddocuments (leadId) VALUES ('${id}')`;
+//     dbConnect.query(
+//       leaddocumentsSql,
+//       (leaddocumentsErr, leaddocumentsResult) => {
+//         if (leaddocumentsErr) {
+//           console.error(
+//             "Error inserting leadId into leaddocuments table:",
+//             leaddocumentsErr
+//           );
+//           // Rollback lead insertion in leads table
+//           const rollbackSql = `DELETE FROM leads WHERE id = '${id}'`;
+//           dbConnect.query(rollbackSql, (rollbackErr) => {
+//             if (rollbackErr) {
+//               console.error(
+//                 "Error rolling back lead insertion in leads table:",
+//                 rollbackErr
+//               );
+//             }
+//             res.status(500).send("Internal Server Error");
+//           });
+//           return;
+//         }
+
+//         res.status(200).send(true);
+//       }
+//     );
+//     res.status(200).send(true);
+//   });
+// });
+
+
 const createLead = asyncHandler((req, res) => {
   let leadId = "L-" + generateRandomNumber(6);
   req.body["leadId"] = leadId;
@@ -177,37 +221,28 @@ const createLead = asyncHandler((req, res) => {
   const sql = `INSERT INTO leads (${createClause[0]}) VALUES (${createClause[1]})`;
   dbConnect.query(sql, (err, result) => {
     if (err) {
-      console.log("Create lead  error:");
+      console.error("Create lead error:", err);
+      res.status(500).send("Internal Server Error");
+      return;
     }
     const id = result.insertId;
     // Insert leadId into leaddocuments table
     const leaddocumentsSql = `INSERT INTO leaddocuments (leadId) VALUES ('${id}')`;
-    dbConnect.query(
-      leaddocumentsSql,
-      (leaddocumentsErr, leaddocumentsResult) => {
-        if (leaddocumentsErr) {
-          console.error(
-            "Error inserting leadId into leaddocuments table:",
-            leaddocumentsErr
-          );
-          // Rollback lead insertion in leads table
-          const rollbackSql = `DELETE FROM leads WHERE id = '${id}'`;
-          dbConnect.query(rollbackSql, (rollbackErr) => {
-            if (rollbackErr) {
-              console.error(
-                "Error rolling back lead insertion in leads table:",
-                rollbackErr
-              );
-            }
-            res.status(500).send("Internal Server Error");
-          });
-          return;
-        }
-
-        res.status(200).send(true);
+    dbConnect.query(leaddocumentsSql, (leaddocumentsErr, leaddocumentsResult) => {
+      if (leaddocumentsErr) {
+        console.error("Error inserting leadId into leaddocuments table:", leaddocumentsErr);
+        // Rollback lead insertion in leads table
+        const rollbackSql = `DELETE FROM leads WHERE id = '${id}'`;
+        dbConnect.query(rollbackSql, (rollbackErr) => {
+          if (rollbackErr) {
+            console.error("Error rolling back lead insertion in leads table:", rollbackErr);
+          }
+          res.status(500).send("Internal Server Error");
+        });
+        return;
       }
-    );
-    res.status(200).send(true);
+      res.status(200).send(true);
+    });
   });
 });
 
